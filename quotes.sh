1c422a9933f5a95851e9f8860760ac3dd2f24cb6
Caroline Richards <caroline.piper.richards@gmail.com>
2024-10-17 23:43:33 -0400

    Add more colors

--- quotes.sh
+++ quotes.sh
@@ -1,42 +1,59 @@
 #!/bin/bash
 
+#nab terminal width so we can center some text later!
 terminal_width=$(tput cols)
 
-center_text() {
-    text="$1"         
-    text_length=${#text}                 
-    padding=$(( (terminal_width - text_length) / 2 )) 
+quotes=("memento mori" "vulnerant omnes, ultima necat" "per aspera ad astra" "carpe diem" "dum spiro, spero" 
+"fortis fortuna adiuvet" "labor omnia vincit" "errare humanum est" "nesce te ipsum" "acta non verba", "ex nihilo nihil"
+"amor et melle et felle est fecundissimus" "esse quam videri" "a fronte praecipitium a tergo lupi" "spiritus intus alit" "virtus non stemma" 
+"carpe nocte" "nihil verus est, omnem facere licet" "spectemur agendo"
+)
+translations=("remember you will die" "they all wound-- the last kills" "through hardship to the stars" "seize the day" 
+"while I breathe I hope" "fortune favors the brave" "hard work conquers all" "to error is human" "know thyself" "actions not words", "nothing out of nothing"
+"love is rich with both honey and venom" "to be rather than to seem" "a precipe in front, wolves behind" "the spirit nourishes within" "virtue, not garland"
+"seize the night" "nothing is true, everything is permitted" "let us be judged by our actions"
+)
 
-    spaces=""
-    for ((i=0; i<$padding;i++)); do
-        spaces+=" "
-    done
+colors=('\033[38;5;5m' '\033[38;5;6m' '\033[38;5;30m' '\033[38;5;34m' '\033[38;5;49m' '\033[38;5;50m'
+'\033[38;5;51m' '\033[38;5;70m' '\033[38;5;155m ' '\033[38;5;225m'
+)
 
-    # Print the centered text
-    echo -e "$spaces$text"
-}
-
-quotes=("memento mori" "vulnerant omnes, ultima necat" "per aspera ad astra" "carpe diem" "dum spiro, spero" "fortis fortuna adiuvet" "labor omnia vincit" "errare humanum est" "nesce te ipsum" "acta non verba")
-translations=("remember you will die" "they all wound the last kills" "through hardship to the stars" "seize the day" "while I breath I hope" "fortune favors the brave" "hard work conquers all" "to error is human" "know thyself" "actions not words")
-
+#grab a random quote
 length=${#quotes[@]}
 index=$(( (RANDOM % $length) ))
-color='\033[36m'
+
+#grab a random color
+colors_length=${#colors[@]}
+color_index=$(( RANDOM % colors_length ))
+color="${colors[$color_index]}"
 reset='\033[0m'
 
+#format the quote to be all uppercase
 quote=$(echo "${quotes[$index]}" | tr '[:lower:]' '[:upper:]')
+quote=$(echo -e "${quote}")
 translation=${translations[$index]}
+translation=$(echo -e "~$translation~")
 
-quote=$(echo -e "${color} ${quote} ${reset}")
-translation="\t~$translation~\n"
-
-#fill line with stars
+#fill top/bottom lines with stars
 starLine=""
 for ((i=0; i<$terminal_width; i++)); do
     starLine+="*"  
 done
 
+###OUTPUT###
 echo -e "\n$starLine\n"
-center_text "$quote"
-center_text "$translation"
+
+#find correct padding to center quote
+text_length=${#quote}                
+padding=$(( (terminal_width - $text_length) / 2)) 
+
+#create padding
+spaces=""
+for ((i=0; i<$padding;i++)); do
+    spaces+=" "
+done
+
+#print quote
+echo -e "$spaces$color$quote$reset"
+echo -e "$spaces\t$translation"
 echo -e "\n$starLine\n"
