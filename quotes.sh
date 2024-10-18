8ec100897bb779a69578ab94d535063ff583899b
Caroline Richards <caroline.piper.richards@gmail.com>
2024-10-18 15:22:17 -0400

    more quotes

--- quotes.sh
+++ quotes.sh
@@ -6,12 +6,19 @@ terminal_width=$(tput cols)
 quotes=("memento mori" "vulnerant omnes, ultima necat" "per aspera ad astra" "carpe diem" "dum spiro, spero" 
 "fortis fortuna adiuvet" "labor omnia vincit" "errare humanum est" "nesce te ipsum" "acta non verba", "ex nihilo nihil"
 "amor et melle et felle est fecundissimus" "esse quam videri" "a fronte praecipitium a tergo lupi" "spiritus intus alit" "virtus non stemma" 
-"carpe nocte" "nihil verus est, omnem facere licet" "spectemur agendo"
+"carpe nocte" "nihil verus est, omnem facere licet" "spectemur agendo" "de opresso liber" "disce aut discede" "post tenebras lux"
+"aut inveniam viam aut faciam" "non scholae sed vita" "sapere aude" "scientia est lux lucis"
+
+"Ecce! In pictura est puella, nomine Conrelia. Cornelia est puella Romana quae in Italia habitat."
+
 )
 translations=("remember you will die" "they all wound-- the last kills" "through hardship to the stars" "seize the day" 
 "while I breathe I hope" "fortune favors the brave" "hard work conquers all" "to error is human" "know thyself" "actions not words", "nothing out of nothing"
-"love is rich with both honey and venom" "to be rather than to seem" "a precipe in front, wolves behind" "the spirit nourishes within" "virtue, not garland"
-"seize the night" "nothing is true, everything is permitted" "let us be judged by our actions"
+"love is rich with both honey and venom" "to be rather than to seem" "a precipe in front, wolves behind" "spirits grows within" "virtue, not garland"
+"seize the night" "nothing is true, everything is permitted" "let us be judged by our actions" "freedom from opression" "learn or leave" "after the shadows, light"
+"I will either find a way or make one" "not for school but for life" "dare to know" "knowledge is the light of light"
+
+":)"
 )
 
 colors=('\033[38;5;5m' '\033[38;5;6m' '\033[38;5;30m' '\033[38;5;34m' '\033[38;5;49m' '\033[38;5;50m'
@@ -38,7 +45,7 @@ translation=$(echo -e "~$translation~")
 for ((i=0; i<$terminal_width; i++)); do
     echo -ne "\033[38;5;${i}m*\033[0m"
 done
-echo ""
+echo -e "\n"
 
 #find correct padding to center quote
 text_length=${#quote}                
@@ -52,9 +59,9 @@ done
 
 #print quote
 echo -e "$spaces$color$quote$reset"
-echo -e "$spaces\t$translation"
+echo -e "$spaces\t$translation\n"
 
 for ((i=0; i<$terminal_width; i++)); do
     echo -ne "\033[38;5;${i}m*\033[0m"
 done
-echo ""
+echo -e "\n"
