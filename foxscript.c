bfebf1712706a1e5d0c00786b945c17744bd4155
Caroline Richards <70033718+nixandruin@users.noreply.github.com>
2025-04-19 12:33:12 -0400

    fox ascii art

---
+++ foxscript.c
@@ -0,0 +1,28 @@
+#include "DigiKeyboard.h"
+
+void setup() {}
+
+void loop() {
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(500);
+
+  // Open Notepad (Windows)
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(300);
+  DigiKeyboard.print("notepad");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(800);
+
+  // Type fox ASCII
+  DigiKeyboard.print("   _,-='\"-.__               /\\_/\\");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.print("   `-.}       `=._,.-==-._.,  @ @._,");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.print("      `-.__   _,-.   )       _,.-'");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.print("           `\"     G..m-\"^m`m'");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+
+  // Stop execution
+  for (;;) {}
+}
